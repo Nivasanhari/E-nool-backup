fd9d0cad0b21554bc726c16e42b6fb26aa91ba44
Hari-Nivas <86042084+Hari-Nivas@users.noreply.github.com>
2023-08-26 15:34:57 +0530

    deleted

--- app/components/NoDefaultLayout/layout.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NoDefaultLayout = ({ children }) => {
-    return <div>{children}</div>;
-};
-
-export default NoDefaultLayout
--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Page from "./Pages/Userside/landingPage/page"
-export default function Home() {
-  return (
-    <div>
-      <Page/> 
-    </div>
-
-  )
-}
